d63d02696ad23a3104636207152d2ac393291315
José Morales <jmoralz92@gmail.com>
2023-10-19 15:07:54 -0600

    fix cfg

--- update-quarto.py
+++ update-quarto.py
@@ -5,7 +5,7 @@ with open('nbs/_quarto.yml', 'rt') as f:
     cfg = yaml.safe_load(f)
 
 cfg['project']['type'] = 'mintlify'
-cfg['format'] = {'mintlify-md': True}
+cfg['format'] = {'mintlify-md': {'code-fold': True}}
 
 with open('nbs/_quarto.yml', 'wt') as f:
     yaml.dump(cfg, f, allow_unicode=True)
