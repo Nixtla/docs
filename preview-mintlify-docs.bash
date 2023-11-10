eff6b634b952346775607b37d283f07d7a06f4f1
José Morales <jmoralz92@gmail.com>
2023-11-10 12:44:19 -0600

    update preview script

--- preview-mintlify-docs.bash
+++ preview-mintlify-docs.bash
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
+echo "Copying extension"
+mkdir -p nbs/_extensions
+cp -r docs-scripts/mintlify nbs/_extensions
+
+echo "Updating quarto"
+python docs-scripts/update-quarto.py
 
 echo "Running nbdev_docs..."
 nbdev_docs
 echo "nbdev_docs is done"
 
 echo "Running docs-final-formatting.bash..."
-chmod +x docs-final-formatting.bash
-./action_files/docs-final-formatting.bash
+./docs-scripts/docs-final-formatting.bash
 echo "docs-final-formatting.bash is done"
 
 echo "Moving necessary assets..."
 cp nbs/mint.json _docs/mint.json
-cp nbs/imgs/logo/dark.png _docs/dark.png
-cp nbs/imgs/logo/light.png _docs/light.png 
-cp nbs/favicon.svg _docs/favicon.svg
+cp docs-scripts/imgs/* _docs/
 echo "Done moving necessary assets"
 
 cd ./_docs
