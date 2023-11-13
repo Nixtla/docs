f343819f7eaceb0660b1bb5df4d227680f7a3902
José Morales <jmoralz92@gmail.com>
2023-11-13 17:44:50 -0600

    match possible description in markdown image

--- docs_replace_imgs.py
+++ docs_replace_imgs.py
@@ -1,29 +1,21 @@
-import os
-from pathlib import Path
 import re
+from functools import partial
+from pathlib import Path
 
 def main():
-  dir_path = os.path.dirname(os.path.realpath(__file__))
-  docs_path = os.path.abspath(os.path.join(dir_path, '../_docs'))
-  docs_path_obj = Path(docs_path)
-  paths_in_dir = docs_path_obj.glob('**/*.mdx')
-  pattern = r'!\[\]\(((?!https:\/\/).+?)\)'
+    pattern = r'!\[(.*)\]\(((?!https:\/\/).+?)\)'
+    for file in Path('_docs').rglob('*.mdx'):
+        content = file.read_text()
+        parent = str(file.parent.relative_to('_docs'))
+        content = re.sub(pattern, partial(url_replace, parent=parent), content)
+        file.write_text(content)
 
-  for file_path in paths_in_dir:
-    if file_path.is_file():
-      folder_arr = file_path.parts[len(docs_path_obj.parts):-1]
-      content = file_path.read_text()
-      content = re.sub(pattern, url_replace(folder_arr), content)
-      file_path.write_text(content)
-
-def url_replace(folder_arr):
-  def url_replace_helper(match):
-    curr_url = Path(match[1])
-    new_url = str(Path(*folder_arr).joinpath(curr_url))
+def url_replace(match, parent):
+    new_url = f'{parent}/{match[2]}'
     if not new_url.startswith('/'):
       new_url = '/' + new_url
-    return '![](' + str(new_url) + ')'
-  return url_replace_helper
+    return f'![{match[1]}]({new_url})'
+
 
 if __name__ == '__main__':
   main()
