71b377ed8a032d932348575f7aed55d54750023e
José Morales <jmoralz92@gmail.com>
2023-11-13 18:31:08 -0600

    handle multiline description

--- docs_replace_imgs.py
+++ docs_replace_imgs.py
@@ -3,18 +3,17 @@ from functools import partial
 from pathlib import Path
 
 def main():
-    pattern = r'!\[(.*)\]\(((?!https:\/\/).+?)\)'
+    pattern = r'!\[([\s\S]*)\]\(((?!https:\/\/).+?)\)'
     for file in Path('_docs').rglob('*.mdx'):
         content = file.read_text()
         parent = str(file.parent.relative_to('_docs'))
+        if parent == '.':
+           parent = '/'
         content = re.sub(pattern, partial(url_replace, parent=parent), content)
         file.write_text(content)
 
 def url_replace(match, parent):
-    if parent == '.':
-        new_url = match[2]
-    else:
-        new_url = f'{parent}/{match[2]}'
+    new_url = f'{parent}/{match[2]}'
     if not new_url.startswith('/'):
       new_url = '/' + new_url
     return f'![{match[1]}]({new_url})'
