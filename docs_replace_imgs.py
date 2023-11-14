d582fc8eeb68f090ccf2816df7980b5760d473f2
José Morales <jmoralz92@gmail.com>
2023-11-13 18:00:55 -0600

    handle curr_dir as parent

--- docs_replace_imgs.py
+++ docs_replace_imgs.py
@@ -11,7 +11,10 @@ def main():
         file.write_text(content)
 
 def url_replace(match, parent):
-    new_url = f'{parent}/{match[2]}'
+    if parent == '.':
+        new_url = match[2]
+    else:
+        new_url = f'{parent}/{match[2]}'
     if not new_url.startswith('/'):
       new_url = '/' + new_url
     return f'![{match[1]}]({new_url})'
