8c058fc9656be011e4600611167ab29606252aa3
José Morales <jmoralz92@gmail.com>
2023-11-21 11:41:44 -0600

    fix multiline equations

--- docs_replace_imgs.py
+++ docs_replace_imgs.py
@@ -4,10 +4,12 @@ from pathlib import Path
 
 def main():
     pattern = r'!\[([^\]]*)\]\(((?!https:\/\/).+?)\)'
+    equation_pattern = r'(\$\$[^\$]+\$\$)'
     for file in Path('_docs').rglob('*.mdx'):
         content = file.read_text()
         parent = str(file.parent.relative_to('_docs'))
         content = re.sub(pattern, partial(url_replace, parent=parent), content)
+        content = re.sub(equation_pattern, multiline_equations, content)
         file.write_text(content)
 
 def url_replace(match, parent):
@@ -18,6 +20,14 @@ def url_replace(match, parent):
     if not new_url.startswith('/'):
       new_url = '/' + new_url
     return f'![{match[1]}]({new_url})'
+
+
+def multiline_equations(matches):
+    match = matches[1]
+    if '\n' not in match:
+        return match
+    symbols = match.replace('$', '')
+    return f'$$\n{symbols}\n$$'
 
 
 if __name__ == '__main__':
